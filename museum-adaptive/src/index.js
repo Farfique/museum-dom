fe3d5e763f6c93ff6eb928e391b8f8731bd1c1c1
Anna Mikhailova <petunova@gmail.com>
2021-09-29 09:09:44 +0300

    init: folder museum-adaptive from museum

---
+++ museum-adaptive/src/index.js
@@ -0,0 +1,13 @@
+const playerTimeRange = document.querySelector('.player-time-range');
+  
+playerTimeRange.addEventListener('input', function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #ffffff ${value}%, #ffffff 100%)`
+});
+
+const playerVolumeRange = document.querySelector('.player-volume-range');
+  
+playerVolumeRange.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #ffffff ${value}%, #ffffff 100%)`
+})
